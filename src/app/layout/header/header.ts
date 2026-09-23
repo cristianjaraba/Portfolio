ff1860052b69f5e8a0fb4a3141ebf6e3dba0687e
Cristian Jaraba <cristianjaraba@hotmail.com>
2026-09-23 14:57:52 +0200

    feat: complete header and responsive styles

--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { Language } from '../../core/interfaces/language';
 
 @Component({
@@ -8,19 +8,38 @@ import { Language } from '../../core/interfaces/language';
   templateUrl: './header.html',
 })
 export class Header {
+
   languages: Language[] = [
     { code: 'de', label: 'DE' },
     { code: 'en', label: 'EN' },
     { code: 'es', label: 'ES' }
   ];
+
   activeLang: string = 'de';
+
   navItems = [
     { label: 'About me', href: '' },
     { label: 'Skills', href: '' },
     { label: 'Projects', href: '' }
   ];
 
+  @ViewChild('navigation_dialog')
+  dialog!: ElementRef<HTMLDialogElement>;
+
+  @ViewChild('menu')
+  menu!: ElementRef<HTMLElement>;
+
+  rectMenu!: DOMRect;
+
   setLanguage(code: string): void {
     this.activeLang = code;
   }
+
+  openDialog() {
+    this.rectMenu = this.menu.nativeElement.getBoundingClientRect();
+    this.dialog.nativeElement.style.top = `${this.rectMenu.bottom + 15}px`;
+    this.dialog.nativeElement.style.right = `${window.innerWidth - this.rectMenu.right}px`;
+    this.dialog.nativeElement.showModal();
+  }
+
 }
